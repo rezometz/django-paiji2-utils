d4fb5ecec659621ad1fe9ffc7d7e1e5536f8d3fc
Louis-Guillaume DUBOIS <contact@lgdubois.fr>
2015-08-31 22:37:14 +0200

    adds urlize2 test from shoutbox repo -- not working

--- paiji2_utils/tests.py
+++ paiji2_utils/tests.py
@@ -1,3 +1,29 @@
-# from django.test import TestCase
+from django.test import TestCase  # , Client
+# from .templatetags.urlize2 import urlize2
 
-# Create your tests here.
+
+class TagsTest(TestCase):
+    pass
+    # def test_urlize2(self):
+    #     text = 'ftp://wedwd'
+    #     open_tag = '<a target="_blank" title="{text}"\
+    #           href="{text}">'.format(text=text)
+    #     close_tag = '</a>'
+    #     urlized = urlize2(text)
+    #     print urlized
+    #     self.assertTrue(open_tag in urlized)
+    #     self.assertTrue(close_tag in urlized)
+
+    #     text = 'http://wedwd'
+    #     open_tag = '<a href="%s">' % text
+    #     close_tag = '</a>'
+    #     urlized = urlize2(text)
+    #     self.assertTrue(open_tag in urlized)
+    #     self.assertTrue(close_tag in urlized)
+
+    #     text = 'https://wedwd'
+    #     open_tag = '<a href="%s">' % text
+    #     close_tag = '</a>'
+    #     urlized = urlize2(text)
+    #     self.assertTrue(open_tag in urlized)
+    #     self.assertTrue(close_tag in urlized)
