33031fdb3415b2f0b910dc16bb58f20a9c108b53
Louis-Guillaume DUBOIS <contact@lgdubois.fr>
2015-09-01 13:15:44 +0200

    adds SuccessUrlMixin

--- paiji2_utils/views.py
+++ paiji2_utils/views.py
@@ -1,3 +1,55 @@
-# from django.shortcuts import render
+from django.core.urlresolvers import (
+    reverse, resolve, Resolver404, NoReverseMatch
+)
+from django.contrib import messages
 
-# Create your views here.
+
+class SuccessUrlMixin(object):
+    """ A create/update/delete view that manages a
+    form which sends {{ request.META.HTTP_REFERER }} as
+    the POST "next" value, must inherit from
+    this class before the "generic.{Create/Update...}View"
+    in order to forbid a redirection to a list of chosen
+    urls. A default url will be used if necessary.
+    """
+
+    default_url_name = 'home'
+
+    forbidden_url_names = tuple()
+
+    forbidden_urls = tuple()
+
+    def get_forbidden_url_names(self):
+        return self.forbidden_url_names
+
+    def get_forbidden_urls(self):
+        return self.forbidden_urls
+
+    def get_default_url(self):
+        return reverse(self.default_url_name)
+
+    def get_success_url(self):
+
+        if hasattr(self, 'message'):
+            messages.success(self.request, self.message)
+
+        # the next POST value must be
+        # "{{ request.META.HTTP_REFERER }}"
+        try:
+            HTTP_REFERER = self.request.POST['next']
+            # we delete the « http://www.website.domain/ » part
+            path = '/' + '/'.join(
+                HTTP_REFERER.split('/')[3:]
+            )
+            # we keep the «/en/myapp/action/pk/» part
+            match = resolve(path)
+        except (KeyError, Resolver404, NoReverseMatch):
+            return self.get_default_url()
+
+        if (
+            match.url_name not in self.get_forbidden_url_names() and
+            path not in self.get_forbidden_urls()
+        ):
+            return path
+        else:
+            return self.get_default_url()
